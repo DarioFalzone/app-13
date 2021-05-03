163f5feae7b463ae8709f809d32e90fcad5de659
DarioFalzone <darioefalzone95@gmail.com>
2021-05-03 16:24:27 -0300

    P02 - Finish

--- complexOperations.test.js
+++ complexOperations.test.js
@@ -42,55 +42,118 @@ describe('complexOperation - Unit Tests', () => {
     })
   });
 
-  
-  // it('Test - for', () => {
-  //   expect(variable.funcion()).toBe();
-  // })
-
-  // it('Test - for', () => {
-  //   expect(variable.funcion()).toBe();
-  // })
-
-  // it('Test - for', () => {
-  //   expect(variable.funcion()).toBe();
-  // })
-
-  // it('Test - for', () => {
-  //   expect(variable.funcion()).toBe();
-  // })
-
-
-
-
-
-
   describe('calculateArea', () => {
-    it('first test for calculateArea', () => {
+    // PASS
+    it('Square - CalculateArea ok', () => {
+      expect(complexOperations.calculateArea('square', 3, 4)).toEqual(12);
+    })
 
+    it('Triangle - CalculateArea ok', () => {
+        expect(complexOperations.calculateArea('triangle', 4, 10)).toEqual(20);
+    })
+
+    it('Circle - CalculateArea ok', () => {
+        expect(complexOperations.calculateArea('circle', 80, 20)).toBeCloseTo(20106.192982974677);
+    })
+
+    it('Rectangle - CalculateArea ok', () => {
+      expect(complexOperations.calculateArea('rectangle', 7, 2)).toEqual(14);
+    })
+
+    // NOT PASS
+    it('Test for undefined figure', () => {
+      expect(complexOperations.calculateArea()).toBe('undefined is not supported');
+
+    });
+
+    it('test for first wrong measurements', () => {
+      expect(complexOperations.calculateArea('circle', 'dog', 3))
+      .toBe('number1 and number2 should be numbers');
     });
   });
 
   describe('sumGratherThan', () => {
-    it('first test for sumGratherThan', () => {
 
-    });
+    // it('Test - for', () => {
+    //   expect(variable.funcion()).toBe();
+    // })
+
+    // it('Test - for', () => {
+    //   expect(variable.funcion()).toBe();
+    // })
+
+    // it('Test - for', () => {
+    //   expect(variable.funcion()).toBe();
+    // })
+
+    // it('Test - for', () => {
+    //   expect(variable.funcion()).toBe();
+    // })
   });
 
   describe('intersectionBetweenArrays', () => {
     it('first test for intersectionBetweenArrays', () => {
 
     });
+
+    // it('Test - for', () => {
+    //   expect(variable.funcion()).toBe();
+    // })
+
+    // it('Test - for', () => {
+    //   expect(variable.funcion()).toBe();
+    // })
+
+    // it('Test - for', () => {
+    //   expect(variable.funcion()).toBe();
+    // })
+
+    // it('Test - for', () => {
+    //   expect(variable.funcion()).toBe();
+    // })
   });
 
   describe('sortArrayOfObjectsByKey', () => {
     it('first test for sortArrayOfObjectsByKey', () => {
 
     });
+
+    // it('Test - for', () => {
+    //   expect(variable.funcion()).toBe();
+    // })
+
+    // it('Test - for', () => {
+    //   expect(variable.funcion()).toBe();
+    // })
+
+    // it('Test - for', () => {
+    //   expect(variable.funcion()).toBe();
+    // })
+
+    // it('Test - for', () => {
+    //   expect(variable.funcion()).toBe();
+    // })
   });
 
   describe('numberOfOddAndEvenNumbers', () => {
     it('first test for numberOfOddAndEvenNumbers', () => {
 
     });
+
+    // it('Test - for', () => {
+    //   expect(variable.funcion()).toBe();
+    // })
+
+    // it('Test - for', () => {
+    //   expect(variable.funcion()).toBe();
+    // })
+
+    // it('Test - for', () => {
+    //   expect(variable.funcion()).toBe();
+    // })
+
+    // it('Test - for', () => {
+    //   expect(variable.funcion()).toBe();
+    // })
   });
 });
